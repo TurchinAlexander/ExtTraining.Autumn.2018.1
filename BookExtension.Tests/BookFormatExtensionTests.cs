b625703d1fe585201138fb9361c38323e0bded91
Turchin Alexander <alexanderturchin1@gmail.com>
2018-10-29 10:06:28 +0300

    Add NUnit tests to BookExtention class.

--- BookExtension.Tests/BookFormatExtensionTests.cs
+++ BookExtension.Tests/BookFormatExtensionTests.cs
@@ -1,12 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NUnit.Framework;
+
+using BookLibrary;
 
 namespace BookExtension.Tests
 {
-    public class BookFormatExtensionTests
-    {
-    }
+	[TestFixture]
+	public class BookFormatExtensionTests
+	{
+		[Test]
+		public void BookExtention_StandartGlobal_ReturnString()
+		{
+			Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+			string expected = "Jon Skeet, C# in Depth";
+			BookFormatExtension bookExtention = new BookFormatExtension();
+
+			string actual = String.Format(bookExtention, "{0:G}", book);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void BookExtention_StandartGlobalYear_ReturnString()
+		{
+			Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+			string expected = "Jon Skeet, C# in Depth, 2019";
+			BookFormatExtension bookExtention = new BookFormatExtension();
+
+			string actual = String.Format(bookExtention, "{0:GY}", book);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void BookExtention_StandartTitle_ReturnString()
+		{
+			Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+			string expected = "C# in Depth";
+			BookFormatExtension bookExtention = new BookFormatExtension();
+
+			string actual = String.Format(bookExtention, "{0:T}", book);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void BookExtention_ProviderGlobalInversion_ReturnString()
+		{
+			Book book = new Book("C# in Depth", "Jon Skeet", 2019, "Manning", 4, 900, 40);
+			string expected = "C# in Depth, Jon Skeet";
+			BookFormatExtension bookExtention = new BookFormatExtension();
+
+			string actual = String.Format(bookExtention, "{0:GI}", book);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		
+	}
 }
