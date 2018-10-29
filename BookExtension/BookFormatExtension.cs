6dc2eb1e4ca1995b10a56c80aab919ae8cd8c8af
Turchin Alexander <alexanderturchin1@gmail.com>
2018-10-29 09:52:02 +0300

    Add FormatProvider to Book class.

--- BookExtension/BookFormatExtension.cs
+++ BookExtension/BookFormatExtension.cs
@@ -1,12 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Globalization;
+
+using BookLibrary;
 
 namespace BookExtension
 {
-    public class BookFormatExtension
-    {
-    }
+	public class BookFormatExtension : IFormatProvider, ICustomFormatter
+	{
+		public object GetFormat(Type formatType)
+		{
+			if (formatType == typeof(ICustomFormatter))
+				return this;
+			else
+				return null;
+				
+		}
+
+		public string Format(string format, object arg, IFormatProvider formatProvider)
+		{
+			string uformat = format.ToUpper();
+
+			if (uformat != "GI")
+			{
+				try
+				{
+					return HandleOtherFormats(format, arg);
+				}
+				catch (FormatException e)
+				{
+					throw new FormatException(String.Format("The format of '{0}' is invalid.", format), e);
+				}
+			}
+
+			Book book = (Book)arg;
+
+			// Global Inverse
+			return $"{book.Title}, {book.Author}";
+		}
+
+		public string HandleOtherFormats(string format, object arg)
+		{
+			if (arg is IFormattable)
+			{
+				return ((IFormattable)arg).ToString(format, CultureInfo.CurrentCulture);
+			}
+			else if (arg != null)
+			{
+				return arg.ToString();
+			}
+			else
+			{
+				return string.Empty;
+			}
+		}
+		
+	}
 }
